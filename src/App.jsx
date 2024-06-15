47a256031e3bd6a5a10afe56bc565be025cab923
zhouzijie <zhouzijie@cqkqinfo.com>
2024-06-15 11:29:07 +0800

    feat:动态绘制三角形

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import frag from "./glsl/index.frag";
 import vert from "./glsl/index.vert";
 import { createShader, createProgram } from "@utils";
 // 定义三个顶点
-const point = [1.0, 0.5, 0.2, 0.3, 0.6, 0.8];
+const point = [];
 function App() {
   const glRef = useRef(null);
   const programRef = useRef(null);
@@ -17,18 +17,21 @@ function App() {
     //创建着色器程序
     const program = createProgram(gl, vertShader, fragShader);
     programRef.current = program;
-    const a_Position = gl.getAttribLocation(program, "a_Position");
     // 使用刚创建好的着色器程序。
     gl.useProgram(program);
+
+    const a_Position = gl.getAttribLocation(program, "a_Position");
+    // 读取宽高
+    const a_ScreenSize = gl.getAttribLocation(program, "a_ScreenSize");
+    // 给宽高赋值
+    gl.vertexAttrib2f(a_ScreenSize, canvas.width, canvas.height);
     // 创建缓冲区
     const buffer = gl.createBuffer();
 
     // 绑定缓冲区
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     // 将数据写入缓冲区
-    // webgl浮点数占用4个字节， 32位
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(point), gl.STATIC_DRAW);
-    // 缓冲区数据和a_position关联
+
     gl.enableVertexAttribArray(a_Position);
     // 传递数据
     const size = 2; // 每次取两个数据
@@ -41,6 +44,17 @@ function App() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     //用上一步设置的清空画布颜色清空画布。
     gl.clear(gl.COLOR_BUFFER_BIT);
+    canvas.addEventListener("click", (e) => {
+      const x = e.pageX;
+      const y = e.pageY;
+      point.push(x, y);
+      if (point.length % 6 === 0) {
+        // webgl浮点数占用4个字节， 32位
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(point), gl.STATIC_DRAW);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLES, 0, point.length / 2);
+      }
+    });
     // 绘制
     gl.drawArrays(gl.TRIANGLES, 0, 3);
   }, []);
--- src/glsl/index.frag
+++ src/glsl/index.frag
@@ -6,5 +6,5 @@ precision mediump float;
 uniform  vec4 u_FragColor;
 
 void main(){
-  gl_FragColor = vec4(0.9608, 0.0784, 0.0784, 1.0);
+  gl_FragColor = vec4(0.08, 0.33, 0.96, 1.0);
 }
